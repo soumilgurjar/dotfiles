f165a1f69ff04d162c6b56a9386fe15cf4b76fd8
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2022-10-31 23:33:55 +0100

    Eliminate need for virtual environment activation for neomutt on macOS

--- linux/neomutt/.config/mutt/neomutt_startup.sh
+++ linux/neomutt/.config/mutt/neomutt_startup.sh
@@ -1,5 +1,3 @@
 #!/home/linuxbrew/.linuxbrew/bin/zsh
 gpgconf --kill -v gpg-agent
 gpg-agent --pinentry-program=$(brew --prefix)/bin/pinentry --daemon
-# source ~/VirtualEnvs/neomutt_env/bin/activate
-# neomutt
--- macOS/neomutt/.config/mutt/neomutt_startup.sh
+++ macOS/neomutt/.config/mutt/neomutt_startup.sh
@@ -1,5 +1,3 @@
 #!/usr/local/bin/zsh
 gpgconf --kill -v gpg-agent
 gpg-agent --pinentry-program=$(brew --prefix)/bin/pinentry-mac --daemon
-# source ~/VirtualEnvs/neomutt_env/bin/activate
-# neomutt
