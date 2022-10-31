7bb836a3b540c6f50fca10f55742d69279a39788
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2022-10-31 23:22:57 +0100

    Add kitty neovim scrollback; open actions; tab-bar; catppuccin theme; session saving

---
+++ macOS/kitty/.config/kitty/session_save.py
@@ -0,0 +1,48 @@
+#####################
+# session_save.py
+#####################
+
+### IMPORTANT!!!!
+### set the following settings in kitty.conf:
+# startup_session ~/.config/kitty/sessions/session.conf
+# allow_remote_control yes
+# map kitty_mod+s launch ~/.config/kitty/savesess.sh
+
+import os
+import json
+
+dir = os.path.dirname(__file__)
+
+json_path = 'sessions/kitty-session.json'
+sess_dump_path = os.path.join(dir, json_path)
+
+def createSession(output, sessionData):
+    for os_window in sessionData:
+        for tab in os_window['tabs']:
+            output.write('new_tab '+tab['title'])
+            output.write('\n')
+            output.write('cd '+tab['windows'][0]['cwd'])
+            output.write('\n')
+            window_count = 0
+            for window in tab['windows']:
+                tabs = len(tab['windows'])
+                if window['is_focused']:
+                    output.write('focus')
+                    output.write('\n')
+                    window_count+=1
+                if tabs-window_count == 0:
+                    break
+                output.write('launch zsh')
+                output.write('\n')
+                window_count+=1
+
+
+with open(sess_dump_path) as f:
+    sessData = json.load(f)
+    sess_file_path = 'sessions/session.conf'
+    sess_full_path = os.path.join(dir, sess_file_path)
+    s = open(sess_full_path,'w')
+    createSession(s,sessData)
+    s.close()
+
+f.close()
---
+++ macOS/kitty/.config/kitty/session_save.sh
@@ -0,0 +1,2 @@
+kitty @ ls > ~/.config/kitty/sessions/kitty-session.json
+python3 ~/.config/kitty/session_save.py
