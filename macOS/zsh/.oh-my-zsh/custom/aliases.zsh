1576e32386f333864b6b7eaf5642119bd25c2737
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2022-10-31 23:57:45 +0100

    Add zmk aliases; cleanup; exa switch; neovim switch on macOS

--- macOS/zsh/.oh-my-zsh/custom/aliases.zsh
+++ macOS/zsh/.oh-my-zsh/custom/aliases.zsh
@@ -7,44 +7,38 @@ alias switch_bash="chsh -s $(which bash)"
 #To avoid brew python versions accidentally linking against a Pyenv-provided Python, we use a wrapper
 alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
 
-#Python3 and pip3 alias
-# alias pip=/usr/local/bin/pip3
-# alias python=/usr/local/bin/python3
-
 #Euler login with SSH
 alias eu="ssh sgurjar@euler.ethz.ch"
 alias euy="ssh -Y sgurjar@euler.ethz.ch"
 alias eux="ssh -X sgurjar@euler.ethz.ch"
 
 # Source Config files
-alias soz="source ~/.zshrc"
+# alias soza="kitty @ send-text --all soz\n" # TODO: Escape <CR> or \n properly
+alias soz="clear && rm -f '$ZSH_COMPDUMP' && exec zsh"
 alias sob="source ~/.bashrc"
 alias sobp="source ~/.bash_profile"
 alias sot="tmux source-file ~/.tmux.conf"
 
-# Vim related
-alias viz="vim ~/.zshrc"
-alias via="vim ~/.oh-my-zsh/custom/aliases.zsh"
-alias vib="vim ~/.bashrc"
-alias vibp="vim ~/.bash_profile"
-alias viv="vim ~/.vimrc"
-alias vit="vim ~/.tmux.conf"
-alias viw="vim ~/Dropbox/Apps/vimwiki/index.md"
-alias vsc="vim -S ~/.vim/sessions/configurations-session.vim"
-alias vsn="vim -S ~/.vim/sessions/neomutt-session.vim"
-alias vsnp="vim -S ~/.vim/sessions/neomutt-powerline-session.vim"
-alias vsw="vim -S ~/.vim/sessions/vimwiki-session.vim"
-alias vsk="vim -S ~/.vim/sessions/kitty-config.vim"
-alias vscl="vim -S ~/.vim/sessions/cover-letter-jobsearch.vim"
-alias vscv="vim -S ~/.vim/sessions/cv-jobsearch.vim"
+# Nvim related
+alias vi="nvim"
+alias viz="nvim ~/.zshrc"
+alias via="nvim ~/.oh-my-zsh/custom/aliases.zsh"
+alias vib="nvim ~/.bashrc"
+alias vibp="nvim ~/.bash_profile"
+alias viv="nvim ~/.vimrc"
+alias vit="nvim ~/.tmux.conf"
+alias viw="nvim ~/Dropbox/Apps/vimwiki/index.md"
 
 function vvs() {
-  vimdiff ~/.dotfiles/macOS/$1 ~/.dotfiles/linux/$1
+    nvim -d ~/.dotfiles/macOS/$1 ~/.dotfiles/linux/$1
 }
 
 #Terminal navigation
 alias fs="du -sh"
-alias la="ls -la"
+alias lso="/bin/ls"
+alias l="ls -la"
+alias la="ls -a"
+alias ls="exa -x --icons --group-directories-first"
 alias cpwd="pwd | pbcopy"
 alias rm="rm -ir"
 alias rmf="rm -irf"
@@ -64,7 +58,7 @@ alias bs="brew search"
 alias bi="brew install"
 alias bic="brew install --cask"
 alias bl="brew list"
-alias buu="brew upgrade && brew update "
+alias buu="brew update && brew upgrade "
 alias bun="brew uninstall"
 
 # Neomutt related
@@ -72,9 +66,13 @@ alias nm="neomutt"
 alias nme="neomutt -e 'source /Users/sgurjar/.config/mutt/accounts/soumil.gurjar@sam.math.ethz.ch.muttrc' "
 alias nmd="neomutt -e 'source /Users/sgurjar/.config/mutt/accounts/soumil.gurjar@d-one.ai.muttrc' "
 alias nms="~/.config/mutt/neomutt_startup.sh"
-alias nmenv="source ~/VirtualEnvs/neomutt_env/bin/activate && neomutt -e 'source /Users/sgurjar/.config/mutt/accounts/soumilgurjar@gmail.com.muttrc' "
-alias nmeenv="source ~/VirtualEnvs/neomutt_env/bin/activate && neomutt -e 'source /Users/sgurjar/.config/mutt/accounts/soumil.gurjar@sam.math.ethz.ch.muttrc' "
-alias nmdenv="source ~/VirtualEnvs/neomutt_env/bin/activate && neomutt -e 'source /Users/sgurjar/.config/mutt/accounts/soumil.gurjar@d-one.ai.muttrc' "
+
+# ZMK related
+alias zmk="pyenv activate zmk_build && source ~/Github_Repositories/zmk-build/zephyr/zephyr-env.sh && cd ~/Github_Repositories/zmk-config"
+alias lily_left="west build -s /Users/sgurjar/Github_Repositories/zmk-build/app/ -d build/left -b nice_nano_v2 -- -DSHIELD=lily58_left -DZMK_CONFIG='/Users/sgurjar/Github_Repositories/zmk-config/config/' > build_left.log 2>&1"
+alias lily_right="west build -s /Users/sgurjar/Github_Repositories/zmk-build/app/ -d build/right -b nice_nano_v2 -- -DSHIELD=lily58_right -DZMK_CONFIG='/Users/sgurjar/Github_Repositories/zmk-config/config/' > build_right.log 2>&1"
+alias lily_flash_left="cp ~/Github_Repositories/zmk-config/build/left/zephyr/zmk.uf2 /Volumes/NICENANO"
+alias lily_flash_right="cp ~/Github_Repositories/zmk-config/build/right/zephyr/zmk.uf2 /Volumes/NICENANO"
 
 # Git related
 alias glog="git log --color --graph --pretty=format:'%C(yellow)%h%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches"
@@ -86,64 +84,46 @@ alias ga="git add"
 alias gap="git add -p"
 alias gcm="git commit -m"
 alias gd="git df"
+alias grt="git rev-parse --show-toplevel | cd"
 
 # Adds commits and pushes files with the argument as the commit message
 function gcam() {
-  git add .
-  git commit -m "$1"
+    git add .
+    git commit -m "$1"
 }
 # Adds commits and pushes files with the argument as the commit message
 function gcap() {
-  git add .
-  git commit -m "$1"
-  git push
+    git add .
+    git commit -m "$1"
+    git push
 }
 
 # Stow related
-function stow_adopt_test() { stow --adopt -nvSt ~ $1 }
-function stow_adopt() { stow --adopt -vSt ~ $1 }
-function stowth_test() { stow -nvSt ~ $1 }
-function stowth() { stow -vSt ~ $1 }
-function unstow_test() { stow -nvDt ~ $1 }
-function unstow() { stow -vDt ~ $1 }
+function stow_adopt_test() { cd ~/.dotfiles/macOS && stow --adopt -nvSt ~ $1 && cd - }
+function stow_adopt() { cd ~/.dotfiles/macOS && stow --adopt -vSt ~ $1 && cd - }
+function stowth_test() { cd ~/.dotfiles/macOS && stow -nvSt ~ $1 && cd - }
+function stowth() { cd ~/.dotfiles/macOS && stow -vSt ~ $1 && cd - }
+function unstow_test() {  cd ~/.dotfiles/macOS && stow -nvDt ~ $1 && cd - }
+function unstow() {  cd ~/.dotfiles/macOS && stow -vDt ~ $1 && cd - }
 
 # Taskwarrior related
 alias tt="taskwarrior-tui"
 
-# Open directory in finder
-# function pfd() {
-    # osascript 2>/dev/null <<EOF
-#         tell application "Finder"
-#            return POSIX path of (target of window 1 as alias)
-#         end tell
-#     EOF
-# }
-
-# function cdf() {
-#     cd "$(pfd)"
-# }
-
 # Aliases for github scripts
 alias termpdf="~/Github_Repositories/termpdf.py/termpdf.py $1"
-alias mpv_server="~/Github_Repositories/play-with-mpv/play_with_mpv.py $1"
 
 # Aliases for kitty related commands
 alias icat="kitty +kitten icat"
 
 #Commonly accessed directories
-alias dlsc='cd ~/polybox/ETHZ_Material/Fall_2021/DeepLearningInScientificComputing/'
-alias scr='cd /Users/sgurjar/Scratch_Bryant/'
-alias of='cd /Users/sgurjar/Scratch_Bryant/OpenFOAM_Cases/'
+alias scr="cd /Users/sgurjar/Scratch_Bryant/OpenFOAM_Cases/"
+alias of="cd /Users/sgurjar/openfoam/sgurjar-10/run/"
 alias matlab="/Applications/MATLAB_R2021a.app/bin/matlab -nojvm -nodesktop -nodisplay -nosplash"
-alias thesis="/Users/sgurjar/Dropbox/Apps/Overleaf/Doctoral Thesis - Soumil"
-alias wiki="/Users/sgurjar/Dropbox/Apps/vimwiki"
-alias sem="cd /Users/sgurjar/polybox/ETHZ_Material/Spring_2022"
 alias gmsh="/Applications/Gmsh.app/Contents/MacOS/gmsh"
 alias mmgs="/Applications/mmgs_debug"
-alias cases='cd /Users/sgurjar/Scratch_Bryant/OpenFOAM_Cases/Useful_Euler_Cases/'
 alias plover='/Applications/Plover.app/Contents/MacOS/Plover'
 
 # Activate environments with alias
 alias neomutt_env='source ~/VirtualEnvs/neomutt_env/bin/activate'
 alias dlsc_env='source ~/VirtualEnvs/dlsc_env/bin/activate'
-alias vogelwarte_env='source ~/Git_Repositories/vogelwarte/vogelwarte-env/bin/activate'
+alias vogelwarte_env='source ~/VirtualEnvs/vogelwarte_env/bin/activate'
