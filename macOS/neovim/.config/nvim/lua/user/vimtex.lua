2bf80ecb13ee7dbe34efbe695ceab5f320a7a930
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-01-21 14:44:03 +0100

    Remove strange sioyek workaround for neovim on macOS; Add updated Brewfile on ventura update

--- macOS/neovim/.config/nvim/lua/user/vimtex.lua
+++ macOS/neovim/.config/nvim/lua/user/vimtex.lua
@@ -16,7 +16,7 @@ vim.g.vimtex_compiler_latexmk = { build_dir = "build" }
 -- Need to run 'xattr -d com.apple.quarantine /Applications/sioyek.app' for permissions
 -- Or install with 'brew install --cask --no-quarantine sioyek'
 vim.g.vimtex_view_method = "sioyek"
-vim.g.vimtex_view_sioyek_exe = "/Applications/sioyek.app/Contents/MacOS/sioyek"
+-- vim.g.vimtex_view_sioyek_exe = "/Applications/sioyek.app/Contents/MacOS/sioyek"
 
 -- vim.g.tex_flavor = "latex" -- Default tex file format
 -- vim.g.vimtex_view_method = "skim" -- Choose which program to use to view PDF file
