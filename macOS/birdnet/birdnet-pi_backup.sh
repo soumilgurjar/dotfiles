707c205f5823fad60d6e15eca61dd1e613c096e1
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-08-04 18:01:46 +0200

    MacOS::Birdnet: Rsync birdnet.conf-defaults file as well

--- macOS/birdnet/birdnet-pi_backup.sh
+++ macOS/birdnet/birdnet-pi_backup.sh
@@ -4,6 +4,7 @@ echo "Copying BirdNET-PI database and settings"
 
 # Start Copying with rsync
 rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/birdnet.conf "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
+rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/birdnet.conf-defaults "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
 rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/apprise.txt "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
 rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/scripts/birds.db "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/scripts/"
 rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/Charts "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/"
