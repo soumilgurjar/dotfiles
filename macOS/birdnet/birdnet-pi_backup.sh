ad15d799c2ce4411e2df25121c4bf61a5022f531
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-08-07 12:26:55 +0200

    MacOS::Birdnet: Add script for updating timestamps; Update rsync backup script
    Rsync backup has the the human readable -h flag. File comparison is done based on only file-size and not time-stamp.

--- macOS/birdnet/birdnet-pi_backup.sh
+++ macOS/birdnet/birdnet-pi_backup.sh
@@ -2,12 +2,14 @@
 
 echo "Copying BirdNET-PI database and settings"
 
-# Start Copying with rsync
-rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/birdnet.conf "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
-rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/birdnet.conf-defaults "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
-rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/apprise.txt "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
-rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/scripts/birds.db "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/scripts/"
-rsync -aPW -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/Charts "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/"
-rsync -aPW --exclude-from "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/By_Date/exclude_list.txt" -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/By_Date "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/"
+# Start Copying with rsync (Always keep -a flag to maintain permissions and timestamps)
+rsync -ahP -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/birdnet.conf "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
+rsync -ahP -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/birdnet.conf-defaults "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
+rsync -ahP -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/apprise.txt "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/"
+rsync -ahP -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdNET-Pi/scripts/birds.db "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdNET-Pi/scripts/"
+rsync -ahP -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/scripts/update_timestamps_linux.sh "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/scripts/"
+rsync -ahP -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/Charts "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/"
+rsync -ahP --size-only --exclude-from "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/By_Date/exclude_list.txt" -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/By_Date "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/"
+# rsync -ahP --size-only -e "ssh -i $HOME/.ssh/id_birdnetpi" soumilgurjar@mediumplacebirder.local:/home/soumilgurjar/BirdSongs/Extracted/By_Date "$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/"
 
 echo "Done syncing"
---
+++ macOS/birdnet/update_timestamps_macos.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Function to extract date and time from the filename
+get_date_time() {
+	# Get the filename without the path
+	filename=$(basename "$1")
+
+	# Extract date (e.g., 2023-08-04 or 23-08-04) from filename
+	date=$(echo "$filename" | grep -oE '.{10}-birdnet' | sed 's/_/-/g' | sed 's/-birdnet//')
+
+	# Extract time (e.g., 06:32:54) from filename
+	time=$(echo "$filename" | grep -oE '[0-9]{2}:[0-9]{2}:[0-9]{2}')
+}
+
+# Function to update file timestamps and echo the changed filenames
+update_timestamps() {
+	# Combine date and time strings for the 'touch' command on macOS
+	formatted_datetime_touch="${date}${time}"
+
+	# Convert formatted_datetime_touch to the required format ([[CC]YY]MMDDhhmm[.SS])
+	formatted_datetime_touch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$formatted_datetime_touch" "+%Y%m%d%H%M.%S")
+
+	# Combine date and time strings for the 'SetFile' command on macOS
+	formatted_datetime_setfile="${date} ${time}"
+
+	# Convert formatted_datetime_setfile to the required format (MM/DD/YYYY HH:MM:SS)
+	formatted_datetime_setfile=$(date -j -f "%Y-%m-%d %H:%M:%S" "$formatted_datetime_setfile" "+%m/%d/%Y %H:%M:%S")
+
+	# Set 'date modified' of the file using the formatted date and time with 'touch'
+	touch -c -t "$formatted_datetime_touch" "$1"
+
+	# Set 'date created' attribute using 'SetFile' command with the corrected format
+	SetFile -d "$formatted_datetime_setfile" "$1"
+
+	# echo "Timestamps updated for: $1"
+	# echo "Date: $date"
+	# echo "Time: $time"
+	echo "Formatted datetime (touch): $formatted_datetime_touch"
+	echo "Formatted datetime (SetFile): $formatted_datetime_setfile"
+}
+
+# Directory containing the files
+main_directory="$HOME/pCloud Drive/Miscellaneous/Apps/BirdNet-Pi/Detection_Database_Backups/BirdSongs/Extracted/By_Date"
+
+# Find all files in the current directory and its subdirectories containing "birdnet" in the name
+# The IFS (Internal Field Separator) is set to a newline to handle spaces in file names.
+IFS=$'\n'
+# files=$(find "$main_directory" -type f -name "*birdnet*")
+files=$(find "$main_directory" -type f -path "*2023-03-*" -o -path "*2023-06-*" -o -path "*2023-07-*" -o -path "*2023-04-01*" -o -path "*2023-04-02*" -o -path "*2023-04-03*" -o -path "*2023-04-04*" -o -path "*2023-04-05*" -o -path "*2023-04-06*" -name "*-birdnet-*")
+
+# Loop through each file and update its timestamps
+for file in $files; do
+	# Call the function to extract date and time
+	get_date_time "$file"
+
+	# Display the file being processed
+	echo "Processing file: $filename"
+
+	# Call the function to update timestamps and echo the extracted date and time
+	update_timestamps "$file"
+done
