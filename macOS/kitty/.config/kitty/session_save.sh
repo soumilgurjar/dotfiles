ec6b564b751839163021e840dc4b1b031d5d3f35
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-08-08 10:58:52 +0200

    MacOS::Kitty: Remove unnecessary files

--- macOS/kitty/.config/kitty/session_save.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/zsh
-kitty @ ls >~/.config/kitty/sessions/kitty-session.json
-# python3 ~/.config/kitty/session_save.py
-python3 ~/Git_Repositories/kitty-save-session/kitty-convert-dump.py <~/.config/kitty/sessions/kitty-session.json >~/.config/kitty/sessions/session.conf
