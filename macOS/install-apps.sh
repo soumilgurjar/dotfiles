20e8ac5806f448a6717e9cac0a7157df173ee28f
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-08-11 19:41:10 +0200

    MacOS:: Remove install app script

--- macOS/install-apps.sh
+++ macOS/install-apps.sh
@@ -4,7 +4,7 @@ echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "Homebrew works now. Installing apps from Brewfile..."
@@ -19,7 +19,7 @@ echo "Changing default shell to zsh"
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-echo "Setting OSX preferences"
+# echo "Setting OSX preferences"
 # Set OS X preferences
 # Need to figure out how to store osx preferences into a .osx file
 # source .osx
