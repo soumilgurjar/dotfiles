216185ae92af20170068dab77bdb5e329c42d9c7
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-09-21 15:06:03 +0200

    MacOS::Neovim: Switch to personal fork of taskwiki; Add comment for conceal cursor

--- noether/neovim/.config/nvim/lua/plugins/taskwiki.lua
+++ noether/neovim/.config/nvim/lua/plugins/taskwiki.lua
@@ -1,5 +1,5 @@
 return {
-	"tools-life/taskwiki",
+	"soumilgurjar/taskwiki",
 	enabled = true,
 	event = "VeryLazy",
 	config = function()
@@ -7,6 +7,6 @@ return {
 		-- vim.g.taskwiki_suppress_mappings = "yes"
 		vim.g.taskwiki_dont_fold = "yes"
 		vim.g.taskwiki_markup_syntax = "markdown" -- Has no documentation as of 20 Apr 2022
-		vim.g.taskwiki_disable_concealcursor = "no"
+		vim.g.taskwiki_disable_concealcursor = "yes" -- If one wants to conceal, set to empty i.e ""
 	end,
 }
