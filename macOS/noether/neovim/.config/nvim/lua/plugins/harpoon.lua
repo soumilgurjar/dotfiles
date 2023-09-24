8b7913b0a338c640f1ef9eb779f6b3de466e4f6f
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-09-24 11:53:19 +0200

    MacOS::Neovim: Fix harpoon go-to mark bug

--- macOS/noether/neovim/.config/nvim/lua/plugins/harpoon.lua
+++ macOS/noether/neovim/.config/nvim/lua/plugins/harpoon.lua
@@ -15,7 +15,7 @@ return {
 		{ "<leader>ha", "<Cmd>lua require('harpoon.ui').nav_file(4)<CR>", desc = "Go to harpoon mark 4" },
 		{ "<leader>hs", "<Cmd>lua require('harpoon.ui').nav_file(3)<CR>", desc = "Go to harpoon mark 3" },
 		{ "<leader>hd", "<Cmd>lua require('harpoon.ui').nav_file(2)<CR>", desc = "Go to harpoon mark 2" },
-		{ "<leader>hf", "<Cmd>lua require('harpoon.mark').nav_file(1)<CR>", desc = "Go to harpoon mark 1" },
+		{ "<leader>hf", "<Cmd>lua require('harpoon.ui').nav_file(1)<CR>", desc = "Go to harpoon mark 1" },
 		{ "<leader>hn", "<Cmd>lua require('harpoon.ui').nav_next()<CR>", desc = "Go to next harpoon mark" },
 		{ "<leader>hp", "<Cmd>lua require('harpoon.ui').nav_prev()<CR>", desc = "Go to previous harpoon mark" },
 		{ "<leader>sf", "<Cmd>Telescope harpoon marks<CR>", desc = "Show harpoon marks in Telescope" },
