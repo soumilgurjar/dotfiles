58690d52d9415c2731b1880978284da6b49e582d
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2023-12-22 18:04:50 +0100

    MacOS::Neovim: Add leetcode plugin; Remove toggleterm split direction

--- macOS/noether/neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ macOS/noether/neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -5,11 +5,11 @@ return {
 	event = "VeryLazy",
 	config = true,
 	opts = {
-		size = 100,
+		-- size = 100,
 		open_mapping = [[<c-\>]],
 		shading_factor = 2,
-		persist_size = true,
-		direction = "vertical",
+		-- persist_size = true,
+		-- direction = "vertical",
 		float_opts = {
 			border = "curved",
 		},
