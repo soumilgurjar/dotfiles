a0d36123292bf52dcdbe5f4a2581d66f3662247f
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2024-06-05 10:20:45 +0200

    Arch::WSL::Fix issues;Disable octo and origami; stop tracking lazy-lock json

--- linux/simrekarch/neovim/.config/nvim/lua/plugins/origami.lua
+++ linux/simrekarch/neovim/.config/nvim/lua/plugins/origami.lua
@@ -1,6 +1,6 @@
 return {
-	"chrisgrieser/nvim-origami",
-	event = "BufReadPost", -- later or on keypress would prevent saving folds
-	enabled = true,
-	opts = true, -- needed even when using default config
+    "chrisgrieser/nvim-origami",
+    event = "BufReadPost", -- later or on keypress would prevent saving folds
+    enabled = false,
+    opts = true,        -- needed even when using default config
 }
