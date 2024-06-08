83b5585fbd3a4fe243ba84ddba44e939abf94324
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2024-06-08 12:04:29 +0200

    Arch::WSL::Neovim: Remove plugins (none-ls, guard, boole), remove octo file but keep it as LazyExtras

--- linux/simrekarch/neovim/.config/nvim/lua/plugins/origami.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "chrisgrieser/nvim-origami",
-    event = "BufReadPost", -- later or on keypress would prevent saving folds
-    enabled = false,
-    opts = true,        -- needed even when using default config
-}
