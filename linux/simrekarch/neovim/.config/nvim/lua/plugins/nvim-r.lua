d17d5c4092855f775c17766d63225704b0d20053
Soumil Gurjar <95701317+soumilgurjar@users.noreply.github.com>
2024-06-20 10:32:50 +0200

    Arch::WSL::Neovim: Remove unnecessary plugin files

--- linux/simrekarch/neovim/.config/nvim/lua/plugins/nvim-r.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"jalvesaq/Nvim-R",
-	enabled = true,
-	-- event = "VeryLazy",
-	ft = { "R", "r" }, -- Only load plugin for these filetypes
-	config = function()
-		vim.cmd([[let R_assign = 0]]) -- This prevents auto substituion of _ to <-
-	end,
-}
